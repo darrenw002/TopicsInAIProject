a0866b5b4a935314c6d1f01c93ad52daa1b0538c
darrenw002 <77371377+darrenw002@users.noreply.github.com>
2025-03-03 14:56:34 -0800

    Add files via upload
    new openAI api key

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///resources.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    openai.api_key = "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"
+    openai.api_key = "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"
