6bc4bce2de3e3cd8c6d63412f3d8a4e530c39a51
shreyasdeore19 <72646118+shreyasdeore19@users.noreply.github.com>
2025-03-03 13:25:36 -0800

    Update models.py
    Solved the isactive error

--- models.py
+++ models.py
@@ -14,10 +14,33 @@ class Resource(db.Model):
     approved = db.Column(db.Boolean, default=True)  # For user-submitted resources, admin can toggle
 
 # User Model for authentication & bookmarking
+# Inherit from db.Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    # Flask-Login requirements:
+    @property
+    def is_active(self):
+        # For this simple app, return True always
+        return True
+
+    @property
+    def is_authenticated(self):
+        # Return True if the user has valid credentials
+        return True
+
+    @property
+    def is_anonymous(self):
+        # Return True only if the user is not logged in
+        return False
+
+    def get_id(self):
+        # Return the user ID as a string
+        return str(self.id)
+
+    
 
 # Bookmark Model - many-to-many relationship
 class Bookmark(db.Model):
